a7e0bbabac5d605c5e859422b7f16abe0e8c4cf0
hodots <31815005+TuxPenguin09@users.noreply.github.com>
2020-07-16 18:33:12 +0800

    Script changes

--- Assets/Scripts/EndScript.cs
+++ Assets/Scripts/EndScript.cs
@@ -1,38 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
 
+using Debug = UnityEngine.Debug;
+
 public class EndScript : MonoBehaviour
 {
-    private int gameShutdown = 27;
-    public Text Countdown;
+    public GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
         WindowTitleChange.ChangeWindowTitleAtEnd("Hey look people, Its kittypop time :D");
-        StartCoroutine(countdown());
+        var videoPlayer = camera.GetComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.loopPointReached += EndReached;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameShutdown <= 0)
-        {
-            Debug.Log("See ya later");
-            //Application.ForceCrash(1);
-            Application.Quit();
-        }
+
     }
 
-    IEnumerator countdown()
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
     {
-        while (true)
-        {
-            gameShutdown -= 1;
-            Countdown.text = "This game will exit in " + gameShutdown;
-            Debug.Log(gameShutdown);
-            yield return new WaitForSeconds(1);
-        }
+        Debug.Log("See ya!");
+        Application.Quit();
     }
+
 }
